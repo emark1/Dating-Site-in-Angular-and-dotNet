caad079e6c25bb47b2864c13ae2fb33aed0da0cf
emark1 <devel010@umn.edu>
2020-05-06 12:38:15 -0500

    Registration method

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -1,12 +1,13 @@
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using DatingApp.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController
+    public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
         public AuthController(IAuthRepository repo)
@@ -17,7 +18,18 @@ namespace DatingApp.API.Controllers
 
         [HttpPost("register")]
         public async Task<IActionResult> Register(string username, string password) {
-            
+            username = username.ToLower();
+            if (await _repo.UserExists(username)) {
+                return BadRequest("Username already exists");
+            }
+
+            var userToCreate = new User {
+                Username = username
+            };
+
+            var createdUser = await _repo.Register(userToCreate, password);
+
+            return StatusCode(201);
         } 
     }
 }
