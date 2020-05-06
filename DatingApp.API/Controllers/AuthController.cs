ab6dde8a71ecd30b1eaf8a30074c1de01e99d6fa
emark1 <devel010@umn.edu>
2020-05-05 20:26:42 -0500

    Tuesday

---
+++ DatingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController
+    {
+        private readonly IAuthRepository _repo;
+        public AuthController(IAuthRepository repo)
+        {
+            _repo = repo;
+
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(string username, string password) {
+            
+        } 
+    }
+}
